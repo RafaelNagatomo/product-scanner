bcf86a914ad090bd359168901e889ba68bdfcc41
Rafael Nagatomo <rafael.nagatomo@majime-tech.com>
2025-05-21 23:39:19 -0300

    feat: first commit

---
+++ apps/api/src/index.ts
@@ -0,0 +1,12 @@
+import express from 'express'
+import dotenv from 'dotenv'
+
+dotenv.config()
+const PORT = process.env.PORT || 4000
+
+const app = express()
+app.use(express.json())
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}...`)
+})
